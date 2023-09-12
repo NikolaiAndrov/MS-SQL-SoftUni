527c931fe041380a9208a3a0a4d14c9d79f28b8e
NikolaiAndrov <32267207+NikolaiAndrov@users.noreply.github.com>
2023-09-12 14:11:33 +0300

    Delete TriggersAndTransactionsExercises directory

--- TriggersAndTransactionsExercises/DeleteEmployees.sql
+++
@@ -1,6 +0,0 @@
-CREATE TRIGGER tr_DeletedEmployees ON Employees FOR DELETE
-AS
-BEGIN
-INSERT INTO Deleted_Employees (FirstName, LastName, MiddleName, JobTitle, DepartmentId, Salary)
-SELECT FirstName, LastName, MiddleName, JobTitle, DepartmentId, Salary FROM deleted
-END
